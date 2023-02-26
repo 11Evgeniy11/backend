1a5ab71d7257c43f40e08be98a744f2668cb6a7d
Евгений <ishutkin.evgeniy@mail.ru>
2023-02-26 17:20:29 +0300

    Lesson3

---
+++ src/test/java/org/example/lesson3/ExampleTest.java
@@ -0,0 +1,167 @@
+package org.example.lesson3;
+
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import io.restassured.http.Headers;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
+
+public class ExampleTest extends AbstractTest {
+    @Test
+    void getSpecifyingRequestDataTest() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("includeNutrition", "false")
+                .pathParam("id", 716429)
+                .when()
+                .get(getBaseUrl() + "recipes/{id}/information")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getCuisineBritish() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("cuisine", "British")
+                .when()
+                .get(getBaseUrl() + "recipes/complexSearch")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getCuisineItalian() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("cuisine", "Italian")
+                .queryParam("intolerances", "gluten")
+                .when()
+                .get(getBaseUrl() + "recipes/complexSearch")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getCuisineGreek() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("cuisine", "Greek")
+                .queryParam("equipment", "blender")
+                .when()
+                .get(getBaseUrl() + "recipes/complexSearch")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getCuisineIndian() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("cuisine", "Indian")
+                .queryParam("excludeIngredients", "eggs")
+                .when()
+                .get(getBaseUrl() + "recipes/complexSearch")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getPork() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","Pork roast with green beans")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getPotatoPancakes() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","Potato pancakes")
+                .formParam("ingredientList","1 onion")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getMeatDumplings() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","Meat dumplings")
+                .formParam("ingredientList","5 eggs")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getPizza() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","Pizza")
+                .formParam("ingredientList","1 package active dry yeast")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getChicken() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","chicken")
+                .formParam("ingredientList","1 chicken")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
+    }
+
+
+    @Test
+    void addMealTest() {
+        String id = given()
+                .queryParam("apiKey", getApiKey())
+                .body("{\n"
+                        + " \"slot\": 1,\n"
+                        + " \"position\": 1,\n"
+                        + " \"type\": \"INGREDIENTS\",\n"
+                        + " \"value\": {\n"
+                        + " \"ingredients\": [\n"
+                        + " {\n"
+                        + " \"name\": \"1 eggs\"\n"
+                        + " }\n"
+                        + " ]\n"
+                        + " }\n"
+                        + "}")
+                .when()
+                .post("https://api.spoonacular.com/mealplanner/geekbrains/items")
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .get("id")
+                .toString();
+}
